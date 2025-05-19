942459d1e80821279b2ff113b28a4644def69f5b
Daniel Constantin <hello@danielconstantin.net>
2025-05-19 17:37:26 +0300

    Fix PrivateCloud API url

--- packages/frontend/src/api/privatecloud-api.ts
+++ packages/frontend/src/api/privatecloud-api.ts
@@ -4,8 +4,8 @@ import rest from "@feathersjs/rest-client"
 
 const app = feathers()
 
-// const restClient = rest("https://cloud.privatefolio.app")
-const restClient = rest("http://localhost:4004")
+const restClient = rest("https://cloud.privatefolio.app")
+// const restClient = rest("http://localhost:4004")
 
 app.configure(restClient.fetch(window.fetch.bind(window)))
 app.configure(auth({ storage: window.localStorage }))
