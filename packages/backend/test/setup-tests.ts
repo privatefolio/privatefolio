cbadd4e57e32555ba77f829f0e64fc7077d0630b
Daniel Constantin <hello@danielconstantin.net>
2025-07-06 11:35:55 +0300

    Fix test setup

--- packages/backend/test/setup-tests.ts
+++ packages/backend/test/setup-tests.ts
@@ -1,12 +1,9 @@
 import { refetchAssetsIfNeeded } from "src/api/account/assets-api"
 import { sleep } from "src/utils/utils"
 
-export async function setup() {
+export default async function setup() {
   console.log("Setting up tests...")
   const assets = await refetchAssetsIfNeeded()
   console.log(`Tests setup complete. ${assets.length} assets fetched.`)
   await sleep(1_000)
-  return assets
 }
-
-export default setup
